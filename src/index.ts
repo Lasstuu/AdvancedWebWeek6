e07c7596a437a7dd5e15e426750c69be7b94f772
Lasstuu <lassituo@gmail.com>
2024-12-07 01:57:04 +0200

    res status 200 in upload

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ router.post("/upload", async (req:Request, res:Response)=>{
             description: description
         })
         await offer.save();
+        res.status(200);
     }catch(error:any){
         console.error(`Error saving user: ${error}`)
 
